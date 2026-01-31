7b1ba4a8e3663d5cbe98f73138b7949f3eaddde6
Sai Keerthi Rudraraju <rudrarajusaikeerthi31@gmail.com>
2026-01-31 13:27:57 +0530

    Added Task 2 Student Grade Calculator

---
+++ Task2_StudentGradeCalculator/README.md
@@ -0,0 +1,19 @@
+# Task 2 - Student Grade Calculator (Java)
+
+## Description
+This Java program calculates total marks, average percentage, and grade
+based on marks entered by the user for multiple subjects.
+
+## Features
+- Accepts marks for any number of subjects
+- Calculates total and average
+- Assigns grade using conditions
+
+## Concepts Used
+- Scanner
+- Loops
+- Conditional statements
+
+## How to Run
+1. Compile: javac StudentGradeCalculator.java
+2. Run: java StudentGradeCalculator
---
+++ Task2_StudentGradeCalculator/StudentGradeCalculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Enter marks for subject " + i + ": ");
+            int marks = sc.nextInt();
+            totalMarks = totalMarks + marks;
+        }
+
+        double average = (double) totalMarks / subjects;
+
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + average);
+
+        if (average >= 90) {
+            System.out.println("Grade: A");
+        } else if (average >= 75) {
+            System.out.println("Grade: B");
+        } else if (average >= 50) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: Fail");
+        }
+
+        sc.close();
+    }
+}
